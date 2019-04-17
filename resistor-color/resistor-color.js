3403087354aa4aed140983127882881bb2f79059
Felix Wambiri <feloh@MacBook-Pro-3.local>
2019-04-17 17:55:15 +0300

    ft(resistor): map the colors to the numbers

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,9 @@
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
+/**
+ *
+ * @param {string} color
+ */
+const colorCode = (color) => COLORS.indexOf(color)
+
+export { COLORS, colorCode };
